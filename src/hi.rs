33900bc12f7e08f3b06bf0e9a4a73697125cd717
phil <uniphil@gmail.com>
2018-11-05 12:42:03 -0500

    more methods for goertz

--- src/hi.rs
+++ src/hi.rs
@@ -3,6 +3,8 @@
 struct Goertz {
     pub f: f64,
     pub n: usize,
+    cosine: f64,
+    sine: f64,
     coeff: f64,
 }
 
@@ -10,15 +12,19 @@ impl Goertz {
     pub fn new(n: usize, target: f64, rate: u32) -> Goertz {
         let k = (0.5 + (n as f64) * target / (rate as f64)).floor();
         let w = 2.0 * std::f64::consts::PI / (n as f64) * k;
-        let coeff = 2.0 * w.cos();
+        let cosine = w.cos();
+        let sine = w.sin();
+        let coeff = 2.0 * cosine;
         Goertz {
             f: target,
             n,
+            cosine,
+            sine,
             coeff,
         }
     }
 
-    pub fn magnitude(&self, samples: &[f32]) -> f64 {
+    fn qs(&self, samples: &[f32]) -> (f64, f64) {
         assert_eq!(samples.len(), self.n);
         let mut q2 = 0.0;
         let mut q1 = 0.0;
@@ -28,8 +34,24 @@ impl Goertz {
             q2 = q1;
             q1 = q0;
         }
+        (q1, q2)
+    }
+
+    pub fn vector(&self, samples: &[f32]) -> (f64, f64) {
+        let (q1, q2) = self.qs(samples);
+        let real = q1 - q2 * self.cosine;
+        let imag = q2 * self.sine;
+        (real, imag)
+    }
+
+    pub fn magnitude_squared(&self, samples: &[f32]) -> f64 {
+        let (q1, q2) = self.qs(samples);
         let mag_squared = q1.powi(2) + q2.powi(2) - q1 * q2 * self.coeff;
-        mag_squared.sqrt()
+        mag_squared
+    }
+
+    pub fn magnitude(&self, samples: &[f32]) -> f64 {
+        self.magnitude.sqrt()
     }
 }
 
@@ -59,7 +81,7 @@ impl Glt {
         }
     }
 
-    pub fn process(&self, min_samples) -> [(f64, f64); NOTES] {
+    pub fn process(&self) -> [(f64, f64); NOTES] {
         let mut mags: [(f64, f64); NOTES] = unsafe { std::mem::uninitialized() };
         for (i, goertz) in self.goertzes.iter().enumerate() {
             let mag = goertz.magnitude(&self.samples[0..goertz.n]);
